a979d1c5b362e8b3b23b164774704588a49e2b72
Alextraterrestre <alexis.carlier07@gmail.com>
2023-02-10 11:35:49 +0100

    add bootstrap icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="desktopHeader">
+  <div class="navbar">
+    div
+  </div>
+  <div class="col-md-12">
+  
+  </div>
+  <!-- <div class="desktopHeader">
     <div class="desktopHeader__logo">
       <router-link :to="{ name: 'Home' }" class="desktopHeader__links__link"
         ><img class="logo" src="../assets/Logo-blanc.svg" alt="Logo du site"
@@ -24,117 +30,121 @@
     </nav>
   </div>
   <div class="bgBlue">
-  </div>
+  </div> -->
 </template>
 
 <style lang="scss">
-.desktopHeader {
-  height: 100px;
-  width: 100%;
-  background-color: rgba(0, 63, 139, 0.05);
-  backdrop-filter: blur(4px);
-  padding: 0px 48px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: fixed;
-  z-index: 10;
 
-  &__logo {
-    width: 112px;
-    height: 80px;
-  }
 
-  &__links {
-    padding: 0px 48px 0px 0px;
+/*** INITIAL NAVBAR****/
+// .desktopHeader {
+//   height: 100px;
+//   width: 100%;
+//   background-color: rgba(0, 63, 139, 0.05);
+//   backdrop-filter: blur(4px);
+//   padding: 0px 48px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   position: fixed;
+//   z-index: 10;
 
-    &__link {
-      font-family: "Nunito";
-      color: #f5f5f5;
-      text-decoration: none;
-      margin-right: 40px;
-      font-size: 18px;
-      position: relative;
-      transition: ease-out 0.1s;
-      transition-delay: 0.2s;
-    }
+//   &__logo {
+//     width: 112px;
+//     height: 80px;
+//   }
 
-    &__link span {
-      z-index: -1;
-      position: absolute;
-      display: block;
-      height: 3px;
-      width: 200%;
-      filter: blur(1px);
-      background: linear-gradient(90deg, transparent, whitesmoke);
-      border-radius: 25%;
-      left: -200%;
-      top: 110%;
-      transition: ease-out 0.2s;
-      opacity: 0;
-    }
+//   &__links {
+//     padding: 0px 48px 0px 0px;
 
-    &__link:hover span {
-      left: 50%;
-      transition: 0.2s;
-      background-color: whitesmoke;
-      box-shadow: 4px 4px 32px 8px #71B0FF, -4px 4px 32px 8px #71B0FF,
-        -4px -4px 32px 8px #71B0FF, 4px -4px 32px 8px #71B0FF;
-      border-radius: 35%;
-      width: 100%;
-      left: 0%;
-      opacity: 1;
-    }
+//     &__link {
+//       font-family: "Nunito";
+//       color: #f5f5f5;
+//       text-decoration: none;
+//       margin-right: 40px;
+//       font-size: 18px;
+//       position: relative;
+//       transition: ease-out 0.1s;
+//       transition-delay: 0.2s;
+//     }
 
-    &__contact-btn {
-    display: inline-flex;
-    border: solid 2px whitesmoke;
-    border-radius: 8px;
-    text-transform: uppercase;
-    font-weight: 600;
-    padding: 8px 32px;
-    color: whitesmoke;
-    text-decoration: none;
-    font-family: "Nunito";
-    font-size: 18px;
-    position: relative;
-    overflow: hidden;
-    z-index: 1;
-  }
-    &__contact-btn:hover {
-      transition: color 0s linear 0s;
-    }
+//     &__link span {
+//       z-index: -1;
+//       position: absolute;
+//       display: block;
+//       height: 3px;
+//       width: 200%;
+//       filter: blur(1px);
+//       background: linear-gradient(90deg, transparent, whitesmoke);
+//       border-radius: 25%;
+//       left: -200%;
+//       top: 110%;
+//       transition: ease-out 0.2s;
+//       opacity: 0;
+//     }
 
-  &__contact-btn::before {
-    content: "";
-    color: aliceblue;
-    background-color: #0095ff;
-    opacity: 0;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    height: 1px;
-    width: 1px;
-    border-radius: 50%;
-    overflow: hidden;
-    z-index: -1;
-    transition: ease-out 0.3s;
-  }
+//     &__link:hover span {
+//       left: 50%;
+//       transition: 0.2s;
+//       background-color: whitesmoke;
+//       box-shadow: 4px 4px 32px 8px #71B0FF, -4px 4px 32px 8px #71B0FF,
+//         -4px -4px 32px 8px #71B0FF, 4px -4px 32px 8px #71B0FF;
+//       border-radius: 35%;
+//       width: 100%;
+//       left: 0%;
+//       opacity: 1;
+//     }
 
-  &__contact-btn:hover::before {
-    width: 200px;
-    height: 200px;
-    top: -200%;
-    left: -20%;
-    opacity: 1;
-  }
-  }
-}
-.bgBlue {
-  background-color: #003F8B;
-  height: 100px;
-  width: 100%;
-  z-index: -10;
-  position: fixed;
-}
+//     &__contact-btn {
+//     display: inline-flex;
+//     border: solid 2px whitesmoke;
+//     border-radius: 8px;
+//     text-transform: uppercase;
+//     font-weight: 600;
+//     padding: 8px 32px;
+//     color: whitesmoke;
+//     text-decoration: none;
+//     font-family: "Nunito";
+//     font-size: 18px;
+//     position: relative;
+//     overflow: hidden;
+//     z-index: 1;
+//   }
+//     &__contact-btn:hover {
+//       transition: color 0s linear 0s;
+//     }
+
+//   &__contact-btn::before {
+//     content: "";
+//     color: aliceblue;
+//     background-color: #0095ff;
+//     opacity: 0;
+//     position: absolute;
+//     left: 50%;
+//     top: 50%;
+//     height: 1px;
+//     width: 1px;
+//     border-radius: 50%;
+//     overflow: hidden;
+//     z-index: -1;
+//     transition: ease-out 0.3s;
+//   }
+
+//   &__contact-btn:hover::before {
+//     width: 200px;
+//     height: 200px;
+//     top: -200%;
+//     left: -20%;
+//     opacity: 1;
+//   }
+//   }
+// }
+// .bgBlue {
+//   background-color: #003F8B;
+//   height: 100px;
+//   width: 100%;
+//   z-index: -10;
+//   position: fixed;
+// }
+/*** INITIAL NAVBAR****/
 </style>
